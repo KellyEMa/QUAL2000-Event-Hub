13b0b7b1d643db78f933eebb24f75eed13a07dbe
BrynnL <brynn.landry2@student.sl.on.ca>
2026-04-21 09:51:30 -0400

    running locally

--- playwright.config.js
+++ playwright.config.js
@@ -25,13 +25,11 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  webServer: {
+  /*webServer: {
     command: 'npm run start',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
-    stdout: 'pipe',
-    stderr: 'pipe'
-   },
+   },*/
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'http://localhost:3000',
